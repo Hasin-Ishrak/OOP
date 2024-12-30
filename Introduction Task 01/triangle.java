f35afb81cf8d1fb5b09a9c190c2962b7c49d07b1
Hasin Ishrak <168259982+Hasin-Ishrak@users.noreply.github.com>
2024-12-30 20:00:13 +0600

    First problem

---
+++ Introduction Task 01/triangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class triangle {
+  public static void calcu(double base, double height) {
+      double area = 0.5 * base * height;
+      System.out.println(area);
+  }
+
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter The base:");
+      double base = sc.nextDouble();
+      System.out.println("Enter The height:");
+      double height = sc.nextDouble();
+      System.out.print("The area of the triangle is: ");
+      calcu(base, height);
+  }
+}
